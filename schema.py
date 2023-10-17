09a085e3a9e07c6c1249fc916c2eed4bb5715a3d
naiyoma <lankas.aurelia@gmail.com>
2023-10-17 17:11:15 +0300

    wallet create endpoint

--- schema.py
+++ schema.py
@@ -6,3 +6,14 @@ class TestUserSchema(BaseModel):
 
     class Config:
         orm_mode = True
+
+class WalletCreateSchema(BaseModel):
+    phone_number: str
+    withdrawalFee: int
+
+class WalletResponseSchema(BaseModel):
+    id: str
+    fiatBalance: str
+    btcBalance: int
+    lightingAddress: str
+    withdrawalFee: int
