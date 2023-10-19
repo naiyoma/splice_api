9307274ab26acc914489188846769af8a7a2d54c
Vladimir Fomene <vladimirfomene@gmail.com>
2023-10-19 16:28:29 +0300

    feat: add more endpoints

--- main.py
+++ main.py
@@ -13,14 +13,25 @@ from schema import WalletCreateSchema
 from schema import WalletResponseSchema
 from schema import BalanceResponse
 from schema import InvoiceRequestSchema
-from schema import WalletDetailResponseSchema
 from schema import InvoiceResponseSchema
+from schema import PaymentRequestSchema
+from schema import PaymentResponseSchema
+from schema import ClaimPaymentResponseSchema
+from schema import RateResponseSchema
+from schema import CalculateRequestSchema
+from schema import CalculateResponseSchema
+from schema import RampInvoiceRequestSchema
+from schema import RampInvoiceResponseSchema
+from schema import RampPaymentRequestSchema
+from schema import RampPaymentResponseSchema
 
 from sqlalchemy.orm import Session
 from database import SessionLocal
-from models import Wallet, Balance, CurrencyEnum
+from models import Wallet, Balance, CurrencyEnum, Asset
 
 from sqlalchemy.orm import Session
+
+from assets import create_asset, finalize_asset_creation, burn_asset, generate_address, send_asset, list_assets
 
 
 from dotenv import load_dotenv
@@ -41,7 +52,15 @@ def get_db():
         db.close()
 
 DEFAULT_FIAT_BALANCE = 100000.0
-DEFAULT_BTC_BALANCE = 5.0
+DEFAULT_BTC_BALANCE = 2.0
+rates = {
+    "NGN/KES": 0.20,
+    "KES/NGN": 5.11,
+    "BTC/KES": 4235589.39,
+    "KES/BTC": 0.000000236,
+    "BTC/NGN": 21644172.60,
+    "NGN/BTC": 0.000000046,
+}
 
 @app.post('/api/invoices', response_model=InvoiceResponseSchema)  # Use the response schema
 async def create_invoice(invoice_request: InvoiceRequestSchema,  db: Session = Depends(get_db)):
@@ -57,16 +76,118 @@ async def create_invoice(invoice_request: InvoiceRequestSchema,  db: Session = D
     
     amount_to_send = invoice_request.amount + receiver_wallet.withdrawal_fee
     amount_to_pay = forex(amount_to_send, sender_wallet.preferred_fiat_currency, receiver_wallet.preferred_fiat_currency)
+    address = generate_address(receiver_wallet.lightning_address, amount_to_send)
     response_data = {
-        "invoice": "",
+        "invoice": address['encoded'],
         "amount": amount_to_pay,
         "currency": sender_wallet.preferred_fiat_currency,
     }
     return response_data
+
+@app.post('/api/invoices/send', response_model=PaymentResponseSchema)  # Use the response schema
+async def pay_invoice(payment_request: PaymentRequestSchema,  db: Session = Depends(get_db)):
+    # pay invoice
+    payment_res = send_asset(payment_request.tapdAddress)
+    # deduct amount from sender wallet balance
+    sender_wallet = db.query(Wallet).filter(Wallet.lightning_address == payment_request.sourceAddress).first()
+    receiver_wallet = db.query(Wallet).filter(Wallet.lightning_address == payment_request.destinationAddress).first()
+    if sender_wallet is None or receiver_wallet is None:
+        raise HTTPException(status_code=404, detail="Wallet not found")
+
+    for balance in sender_wallet.balances:
+        if balance.currency.value != 'BTC':
+            balance.amount -= payment_request.amount
+
+    for balance in receiver_wallet.balances:
+        if balance.currency.value != 'BTC':
+            balance.amount += forex(payment_request.amount, receiver_wallet.preferred_fiat_currency, sender_wallet.preferred_fiat_currency)
+
+    db.add(sender_wallet)
+    db.add(receiver_wallet)
+    db.commit()
+    # burn equivalent amount of NGN asset
+    assets = list_assets()
+    asset_id = ''
+    for asset in assets['assets']:
+        if asset['asset_genesis']['name'] == sender_wallet.preferred_fiat_currency:
+            asset_id = asset['asset_genesis']['asset_id']
+    asset = burn_asset(asset_id, payment_request.amount)
+    # mint equivalent amount of KES asset
+    create_asset(receiver_wallet.preferred_fiat_currency, forex(payment_request.amount, receiver_wallet.preferred_fiat_currency, sender_wallet.preferred_fiat_currency))
+    finalize_asset_creation()
+
+    response_data = {
+        "proofOfPayment": payment_res['transfer']['anchor_tx_hash'],
+        "amount": payment_request.amount,
+        "currency": sender_wallet.preferred_fiat_currency,
+        "destinationAddress": payment_request.destinationAddress,
+    }
+    return response_data
+
+@app.get('/api/invoices/<txid>', response_model=ClaimPaymentResponseSchema)
+async def claim_payment(txid: str):
+    # check if there is any asset transfer with this ID
+    # if there is return true if there is not return false
+    transfers = get_transfers()
+    for transfer in transfers['transfers']:
+        if transfer['transfer']['anchor_tx_hash'] == txid:
+            return {
+                "succeeded": True
+            }
+    return {
+        "succeeded": False
+    }
+
+@app.get('/api/rate/<source>/<destination>', response_model=RateResponseSchema)
+async def get_rate(source: str, destination: str):
+    rate_key = f"{source}/{destination}"
+    return {
+        "rate": rates[rate_key]
+    }
+
+@app.post('/api/on/calculate', response_model=CalculateResponseSchema)
+async def amount_to_receive(req: CalculateRequestSchema):
+    to_receive = req.rate * req.amount
+    return {
+        "amount": to_receive,
+        "currency": req.destination
+    }
+
+@app.post('/api/on/invoices', response_model=RampInvoiceResponseSchema)
+async def get_ramp_invoice(req: RampInvoiceRequestSchema):
+    assets = list_assets()
+    asset_id = ''
+    for asset in assets['assets']:
+        if asset['asset_genesis']['name'] == req.currency:
+            asset_id = asset['asset_genesis']['asset_id']
+    address = generate_address(asset_id, req.amount)
+
+    return {
+        "destinationAddress": address['encoded'],
+    }
+
+@app.post('/api/on/invoices/send', response_model=RampPaymentResponseSchema)
+async def pay_ramp_invoice(req: RampPaymentRequestSchema, db: Session = Depends(get_db)):
+    payment_res = send_asset(req.tapdAddress)
+    receiver_wallet = db.query(Wallet).filter(Wallet.lightning_address == req.lightningAddress).first()
+    if receiver_wallet is None:
+        raise HTTPException(status_code=404, detail="Wallet not found")
+
+    for balance in receiver_wallet.balances:
+        if balance.currency.value == req.currency:
+            balance.amount += req.amount
     
+    db.add(receiver_wallet)
+    db.commit()
+
+    return {
+        "paymentId": payment_res['transfer']['anchor_tx_hash'],
+        "status": "Payment successful"
+    }
+
 
 @app.post('/api/wallets', response_model=WalletResponseSchema)  # Use the response schema
-async def create_wallet(wallet: WalletCreateSchema):
+async def create_wallet(wallet: WalletCreateSchema, db: Session = Depends(get_db)):
 
     new_wallet = Wallet(
         phone_number=wallet.phoneNumber,
@@ -75,8 +196,58 @@ async def create_wallet(wallet: WalletCreateSchema):
         lightning_address=wallet.phoneNumber + "@splice.africa",
     )
 
-    db.session.add(new_wallet)
-    db.session.commit()
+    db.add(new_wallet)
+    db.commit()
+
+    fiat_asset = create_asset(wallet.preferredFiatCurrency, DEFAULT_FIAT_BALANCE)
+    btc_asset = create_asset("BTC", DEFAULT_BTC_BALANCE)
+    finalize_asset_creation()
+
+    existing_fiat_asset = db.query(Asset).filter(Asset.currency == CurrencyEnum(wallet.preferredFiatCurrency)).first()
+
+   
+
+    if existing_fiat_asset:
+        # If an asset with the same name exists, update it
+        existing_fiat_asset.amount += DEFAULT_FIAT_BALANCE
+        db.add(existing_fiat_asset)
+    else:
+        # If not, create a new asset
+        # Sleep for 30 seconds, so that asset gets confirmed
+        time.sleep(35)
+        assets = list_assets()
+        asset_id = ''
+        for asset in assets['assets']:
+            if asset['asset_genesis']['name'] == wallet.preferredFiatCurrency:
+                asset_id = asset['asset_genesis']['asset_id']
+        new_asset = Asset(
+            amount=DEFAULT_FIAT_BALANCE,
+            asset_id=asset_id,
+            currency=CurrencyEnum(wallet.preferredFiatCurrency)
+        )
+        db.add(new_asset)
+
+    existing_btc_asset = db.query(Asset).filter(Asset.currency == CurrencyEnum('BTC')).first()
+
+    if existing_btc_asset:
+        # If an asset with the same name exists, update it
+        existing_btc_asset.amount += btc_asset["amount"]
+        db.add(existing_btc_asset)
+    else:
+        # If not, create a new asset
+        # Sleep for 30 seconds, so that asset gets confirmed
+        time.sleep(35)
+        assets = list_assets()
+        asset_id = ''
+        for asset in assets['assets']:
+            if asset['asset_genesis']['name'] == 'BTC':
+                asset_id = asset['asset_genesis']['asset_id']
+        new_asset = Asset(
+            amount=DEFAULT_BTC_BALANCE,
+            asset_id=asset_id,
+            currency=CurrencyEnum('BTC')
+        )
+        db.add(new_asset)
 
     fiat_balance = Balance(
         amount=DEFAULT_FIAT_BALANCE,
@@ -90,9 +261,9 @@ async def create_wallet(wallet: WalletCreateSchema):
         wallet_id=new_wallet.id
     )
 
-    db.session.add(fiat_balance)
-    db.session.add(btc_balance)
-    db.session.commit()
+    db.add(fiat_balance)
+    db.add(btc_balance)
+    db.commit()
 
     response_data = {
         "id": new_wallet.id,
@@ -105,7 +276,7 @@ async def create_wallet(wallet: WalletCreateSchema):
     }
     return response_data
 
-@app.get('/api/wallets/{wallet_id}', response_model=WalletDetailResponseSchema)
+@app.get('/api/wallets/{wallet_id}', response_model=WalletResponseSchema)
 async def get_wallet_by_id(
     wallet_id: str = Path(..., title="The ID of the wallet to retrieve"),
     db: Session = Depends(get_db)
@@ -115,10 +286,9 @@ async def get_wallet_by_id(
         raise HTTPException(status_code=404, detail="Wallet not found")
     
     response_data = {
-        "walletID": db_wallet.id,
-        "fiatBalance": fiat_balance,
-        "btcBalance": btc_balance,
-        "lightingAddress": f"{db_wallet.phone_number}@splice.africa",
+        "id": db_wallet.id,
+        "balances": db_wallet.balances,
+        "lightingAddress": db_wallet.lightning_address,
         "withdrawalFee": db_wallet.withdrawal_fee,
     }
     return response_data
@@ -132,14 +302,6 @@ def forex(amount_to_send, sender_fiat_currency, receiver_fiat_currency):
     if sender_fiat_currency == receiver_fiat_currency:
         raise HTTPException(status_code=400, detail="Sender and receiver fiat currencies cannot be the same")
     rate_key = f"{receiver_fiat_currency}/{sender_fiat_currency}"
-    rates = {
-        "NGN/KES": 0.20,
-        "KES/NGN": 5.11,
-        "BTC/KES": 4235589.39,
-        "KES/BTC": 0.000000236,
-        "BTC/NGN": 21644172.60,
-        "NGN/BTC": 0.000000046,
-    }
 
     return amount_to_send * rates[rate_key]
 
